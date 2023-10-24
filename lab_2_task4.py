d37662be065620212a4d94810fea1dac0c72453f
andrey1bkv <146856388+andrey1bkv@users.noreply.github.com>
2023-10-24 16:48:45 +0000

    less3

---
+++ lab_2_task4.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+n = int(input("введите колличество чисел"))
+for i in range(n)
+    b = a + b
+    print(b)
+    a = a + b
+    c = a + b
+    d = a + c 
+    print(d)
+    
+
+ 
