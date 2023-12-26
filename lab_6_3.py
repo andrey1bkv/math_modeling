86d77387b28b63cf92e8b581f96ad5a6a4fcbdce
andrey1bkv <146856388+andrey1bkv@users.noreply.github.com>
2023-12-26 16:52:31 +0000

    errfefe

--- lab_6_3.py
+++ lab_6_3.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-def ellips_plotter(a=1, b=2,):
-    x = np.arange(-10, 10,)
-    y = 1-x*b/a
-
-    plt.plot(x, y, label='Andreya ellips')
-    plt.title('Ellips')
-    plt.legend() 
-    plt.savefig('lab_3.png')  
+def ellips_plotter(a=150, b=90):
+     x = a*cos(t)
+     y = b*sin(t)
+     x = np.arange(10, -10)
+     x = np.arange(20, -20)
+     plt.plot(x, y, label='Andreya ellips')
+     plt.title('Ellips')
+     plt.legend() 
+     plt.savefig('lab_3.png')  
 
 if __name__ == '__main__':      
 	ellips_plotter
