395bda36ce504f41877efcf71036997385ab09bc
andrey1bkv <146856388+andrey1bkv@users.noreply.github.com>
2023-12-26 15:15:58 +0000

    rfrfg

--- lab_6_3.py
+++ lab_6_3.py
@@ -1,5 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-def ellips_plotter(a=1, b=1, c=0):
-    x = np.arange(-10, 10, 0.01)
-    y = a*x**2 + b*x
+def ellips_plotter(a=1, b=2,):
+    x = np.arange(-10, 10,)
+    y = 1-x*b/a
+
+    plt.plot(x, y, label='Andreya ellips')
+    plt.title('Ellips')
+    plt.legend() 
+    plt.savefig('lab_3.png')  
+
+if __name__ == '__main__':      
+	ellips_plotter
+                
+
+
+
+
